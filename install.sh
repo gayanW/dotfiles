a9f959fdf57530c4e0e867adeb650d6838259cf5
Gayan Weerakutti <me@debian.gayanw.local>
2017-05-07 13:31:53 +0530

    Add xinitrc

--- install.sh
+++ install.sh
@@ -6,5 +6,9 @@ BASE=$(pwd)
 sudo apt-get install fonts-inconsolata
 
 # .Xresources
+mv -v ~/.xinitrc ~/.xinitrc.old 2> /dev/null
+ln -sf $BASE/xinitrc  ~/.xinitrc
+
+# .Xresources
 mv -v ~/.Xresources ~/.Xresources.old 2> /dev/null
 ln -sf $BASE/Xresources ~/.Xresources
