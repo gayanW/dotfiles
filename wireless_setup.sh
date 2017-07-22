74ae0627d55c197e3666654971e37bbef6b761f1
Gayan Weerakutti <gayan@linuxdeveloper.space>
2017-07-22 13:12:05 +0530

    Add wireless_setup.sh

---
+++ wireless_setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo apt-get install wpasupplicant
+
+echo
+echo "To connect to a wlan see: https://wiki.debian.org/WiFi/HowToUse#wpa_supplicant"
+
+# GUI
+# To start wicd run $ wicd -n
+#sudo apt-get install wicd
+
+# Useful commands
+# iup wlan0
+# wpa_cli terminate
