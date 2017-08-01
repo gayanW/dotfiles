7b276b2506204bdb6c329c588eaad0a68b7d0fd9
Gayan Weerakutti <gayan@linuxdeveloper.space>
2017-08-01 12:03:50 +0530

    Add multimedia.sh

---
+++ multimedia.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd $(dirname $BASH_SOURCE)
+BASE=$(pwd)
+
+sudo apt-get update
+sudo apt-get install audacity
+sudo apt-get install lightworks
+sudo apt-get install -t jessie-backports youtube-dl
+
+echo "Install vokoscreen screen recorder from: "
+echo "http://linuxecke.volkoh.de/vokoscreen/vokoscreen-download.html"
