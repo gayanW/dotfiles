6f628ff89c4111d09ea6a6c0d726be3c0aa4635d
Gayan Weerakutti <gayan@linuxdeveloper.space>
2017-10-23 20:54:31 +0530

    Update install.sh to sign commits using GPG

--- install.sh
+++ install.sh
@@ -20,3 +20,8 @@ sudo apt-get install git
 git config --global user.email "gayan@linuxdeveloper.space"
 git config --global user.name "Gayan Weerakutti"
 git config --global core.editor "vim"
+
+# Signing commits using GPG
+git config --global user.signingkey 626BE6CD
+# To set all commits to be signed by defaul
+git config --global commit.gpgsign true
