ffabd4e0b440f9ba11ca1a3f2bff74d1180393a7
Gayan Weerakutti <gayan@linuxdeveloper.space>
2017-11-25 21:40:39 +0530

    Seperate git_install.sh

--- install.sh
+++ install.sh
@@ -14,14 +14,3 @@ ln -sf $BASE/xinitrc  ~/.xinitrc
 mv -v ~/.Xresources ~/.Xresources.old 2> /dev/null
 ln -sf $BASE/Xresources ~/.Xresources
 sudo apt-get install fonts-inconsolata
-
-# git-config
-sudo apt-get install git
-git config --global user.email "gayan@linuxdeveloper.space"
-git config --global user.name "Gayan Weerakutti"
-git config --global core.editor "vim"
-
-# Signing commits using GPG
-git config --global user.signingkey 626BE6CD
-# To set all commits to be signed by defaul
-git config --global commit.gpgsign true
