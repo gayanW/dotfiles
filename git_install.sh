cd6e82201c7cb94db45a9a172892609b28bf9eaa
Gayan Weerakutti <gayan@linuxdeveloper.space>
2017-11-25 21:57:17 +0530

    Add GPG key check to git_install

--- git_install.sh
+++ git_install.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-# git-config
 sudo apt-get install git
+
+# git-config
 git config --global user.email "gayan@linuxdeveloper.space"
 git config --global user.name "Gayan Weerakutti"
 git config --global core.editor "vim"
 
-# Signing commits using GPG
-git config --global user.signingkey 626BE6CD
-# To set all commits to be signed by defaul
-git config --global commit.gpgsign true
+if gpg --list-keys 626BE6CD; then
+	# Signing commits using GPG
+	git config --global user.signingkey 626BE6CD
+	# To set all commits to be signed by defaul
+	git config --global commit.gpgsign true
+else
+	echo "GPG Key 626BE6CD not found."
+fi
