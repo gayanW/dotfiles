51f81b12791268b9f5f3c60a88e9d43a8cd249ea
Gayan Weerakutti <gayan@linuxdeveloper.space>
2017-12-06 12:45:47 +0530

    synaptics update for debian 9

--- synaptics/synaptics.sh
+++ synaptics/synaptics.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-sudo mkdir /etc/X11/xorg.conf.d/
-sudo cp /usr/share/X11/xorg.conf.d/50-synaptics.conf /etc/X11/xorg.conf.d/50-synaptics.conf
+sudo mkdir -p /etc/X11/xorg.conf.d/
+
+VERSION=$(cat /etc/debian_version)
+
+# Debian 9 "Stretch" is using libinput driver 
+sudo cp /usr/share/X11/xorg.conf.d/40-libinput.conf /etc/X11/xorg.conf.d/40-libinput.conf
+
+#sudo cp /usr/share/X11/xorg.conf.d/50-synaptics.conf /etc/X11/xorg.conf.d/50-synaptics.conf
+
 echo "To configure see: https://unix.stackexchange.com/questions/337008/activate-tap-to-click-on-touchpad"
+echo "To configure see: https://wiki.debian.org/SynapticsTouchpad#Enable_tapping_on_touchpad"
