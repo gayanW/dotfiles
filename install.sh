ae9929906a54bf6c929ccfeb789adf4215d2f311
Gayan Weerakutti <gayan@linuxdeveloper.space>
2018-01-12 18:12:58 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,11 +6,12 @@ BASE=$(pwd)
 sudo apt-get install xinit
 sudo apt-get install x11-xserver-utils
 
-# .Xresources
+# .xinitrc (not required)
 mv -v ~/.xinitrc ~/.xinitrc.old 2> /dev/null
-ln -sf $BASE/xinitrc  ~/.xinitrc
+cp $BASE/xinitrc  ~/.xinitrc
 
 # .Xresources
 mv -v ~/.Xresources ~/.Xresources.old 2> /dev/null
-ln -sf $BASE/Xresources ~/.Xresources
+cp $BASE/Xresources ~/.Xresources
+
 sudo apt-get install fonts-inconsolata
