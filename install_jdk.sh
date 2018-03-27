16a1fa49006174a494c899b081db1b905b648558
Gayan Weerakutti <gayan@linuxdeveloper.space>
2018-03-27 11:34:55 +0530

    Install from Debian stable instead of backports

--- install_jdk.sh
+++ install_jdk.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-sudo apt-get purge openjdk-7-*
+sudo apt-get purge openjdk-*
 
-sudo apt-get install -t jessie-backports openjdk-8-jdk
+# if the latest jdk does not exists
+# install from backports
+sudo apt-get install openjdk-8-jdk
